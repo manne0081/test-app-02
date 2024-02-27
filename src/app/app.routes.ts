6e13b9e441f7dc36dd5bb14115e9ad64f0de567e
manne0081@gmail.com <manne0081@gmail.com>
2024-02-27 09:14:06 +0100

    test-1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PrivateComponent } from './private/private.component';
+
+export const routes: Routes = [
+    { path: '',   redirectTo: 'home', pathMatch: 'full' },
+
+    { path: 'home', component: HomeComponent },
+    { path: 'private', component: PrivateComponent },
+
+    { path: '**', component: AppComponent },
+];
