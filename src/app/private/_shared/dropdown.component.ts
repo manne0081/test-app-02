26e1d7e5e67a4f35218204dd97268cf663c18423
manne0081@gmail.com <manne0081@gmail.com>
2024-07-19 09:49:38 +0200

    dropdown-test

--- src/app/private/_shared/dropdown.component.ts
+++ src/app/private/_shared/dropdown.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit, OnDestroy, HostListener, Input } from '@angular/core
 import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { DropdownService } from './dropdown.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-dropdown',
     standalone: true,
     imports: [
         CommonModule,
+        FormsModule,
     ],
     templateUrl: './dropdown.component.html',
     styleUrl: './dropdown.component.scss'
@@ -18,7 +20,14 @@ export class DropdownComponent implements OnInit, OnDestroy{
     @Input() dropdownId: string = '';
 
     showDropContent: boolean = false;
+
+    showFieldSelect: boolean = false;
+    showOperatorSelect: boolean = false;
+    filterValue: string = '';
+
     private subscription: Subscription = new Subscription();
+
+    private static openedDropdownId: string | null = null;
 
     constructor(
         private dropdownService: DropdownService,
@@ -48,4 +57,37 @@ export class DropdownComponent implements OnInit, OnDestroy{
             this.dropdownService.setOpenDropdownId(null);
         }
     }
+
+
+
+    openSelect(type: string): void {
+        if (type === 'field') {
+            this.showFieldSelect = !this.showFieldSelect;
+            this.showOperatorSelect = false;
+        } else if (type === 'operator') {
+            this.showOperatorSelect = !this.showOperatorSelect;
+            this.showFieldSelect = false;
+        }
+    }
+
+    selectField(field: string): void {
+        this.showFieldSelect = false;
+        console.log('Selected field:', field);
+    }
+
+    selectOperator(operator: string): void {
+        this.showOperatorSelect = false;
+        console.log('Selected operator:', operator);
+    }
+
+    addCondition(): void {
+        console.log('Add condition clicked');
+    }
+
+    addConditionGroup(): void {
+        console.log('Add condition group clicked');
+    }
+
+
+
 }
